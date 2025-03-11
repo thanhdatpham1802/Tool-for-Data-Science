b5e5c0ff7fdc5297fdca8cec0ab935aeb58a0957
thanhdatpham1802 <feeddepro@gmail.com>
2025-03-11 09:20:29 +0700

    Add files via upload

---
+++ DataScienceEcosystem.ipynb.py
@@ -0,0 +1,63 @@
+# Data Science Tools and Ecosystem
+
+## Introduction
+In this notebook, Data Science Tools and Ecosystem are summarized.
+
+## Popular Data Science Languages
+Some of the popular languages that Data Scientists use are:
+
+1. Python  
+2. R  
+3. SQL  
+4. Julia  
+5. Scala  
+
+## Commonly Used Data Science Libraries
+Some of the commonly used libraries used by Data Scientists include:
+
+1. NumPy  
+2. Pandas  
+3. Matplotlib  
+4. Scikit-learn  
+5. TensorFlow  
+
+## Data Science Tools
+| Data Science Tools |
+|--------------------|
+| Jupyter Notebook  |
+| RStudio           |
+| Apache Spark      |
+
+### Below are a few examples of evaluating arithmetic expressions in Python.
+import operator
+
+a = 10
+b = 5
+
+print(operator.add(a, b))   # Output: 15
+print(operator.sub(a, b))   # Output: 5
+print(operator.mul(a, b))   # Output: 50
+print(operator.truediv(a, b))  # Output: 2.0
+print(operator.floordiv(a, b))  # Output: 2
+print(operator.mod(a, b))   # Output: 0
+print(operator.pow(a, b))   # Output: 100000
+
+# This is a simple arithmetic expression to multiply then add integers.
+result = (3*4) + 5
+print(result)
+
+
+# This will convert 200 minutes to hours by dividing by 60.
+minutes = 200 
+hours = minutes / 60
+print(hours)
+
+## **Objectives**
+- List popular languages for Data Science  
+- Identify commonly used Data Science libraries  
+- Summarize open-source Data Science tools  
+- Perform arithmetic operations in Python  
+- Convert time units in Python  
+
+## Author
+Pham Thanh Dat
